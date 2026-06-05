7608824df74e52d839a33241eb70c1d4b84d59b9
SydB99 <pi.musco@studenti.unina.it>
2026-06-05 19:03:18 +0200

    Initial commit: core app structure

---
+++ constants/data.js
@@ -0,0 +1,122 @@
+export const center = [41.3392, 14.5225];
+
+export const bounds = L.latLngBounds(
+  [41.22, 14.42], // Sud-Ovest (longitudine più piccola = più a sinistra)
+  [41.44, 14.72]  // Nord-Est (latitudine più grande = più in alto)
+);
+
+export const markers = [
+  {
+    name: "La Leonessa",
+    lat: 41.28017,
+    lng: 14.60068,
+    audio: "../../media/audio/Grotta-Leonessa.mp3",
+    image: "../../media/photos/Grotta_Leonessa.jpg",
+  },
+  {
+    name: "Chiamata A Raccolta",
+    lat: 41.27978,
+    lng: 14.60143,
+    audio: "../../media/audio/Cartello-Installazione.mp3",
+    image: "../../media/photos/Chiamata.jpg",
+  },
+  {
+    name: "Il Braccio Mancante Del Tratturo",
+    lat: 41.28291,
+    lng: 14.60247,
+    audio: "../../media/audio/Pecore-Tratturo.mp3",
+    image: "../../media/photos/Pecore_Tratturo.jpg",
+  },
+  {
+    name: "Le Cicale Di Caccaviola",
+    lat: 41.3495,
+    lng: 14.53359,
+    audio: "../../media/audio/Cicale-di-Caccaviola.mp3",
+    image: "../../media/photos/cicale.jpg",
+  },
+  {
+    name: "I Cavalli",
+    lat: 41.31423,
+    lng: 14.53293,
+    audio: "../../media/audio/I-Cavalli.mp3",
+    image: "../../media/photos/i_cavalli.jpg",
+  },
+  {
+    name: "Le Forre Di Lavello",
+    lat: 41.30069,
+    lng: 14.53821,
+    audio: "../../media/audio/Le-Forre.mp3",
+    image: "../../media/photos/Le_Forre.jpg",
+  },
+  {
+    name: "Il Temporale",
+    lat: 41.32887,
+    lng: 14.49242,
+    audio: "../../media/audio/Il-temporale.mp3",
+    image: "../../media/photos/Il_temporale.jpg",
+  },
+  {
+    name: "Al Bar",
+    lat: 41.33638,
+    lng: 14.51124,
+    audio: "../../media/audio/Al-Bar.mp3",
+    image: "",
+  },
+  {
+    name: "Suonata Cusanese",
+    lat: 41.34007,
+    lng: 14.50811,
+    audio: "../../media/audio/Suonata-Cusanese_1.mp3",
+    image: "../../media/photos/Suonata_cusanese.jpg",
+  },
+  {
+    name: "Campanacci - Freestyle",
+    lat: 41.32219,
+    lng: 14.5112,
+    audio: "../../media/audio/Campanacci.mp3",
+    image: "../../media/photos/Campanaccio.jpg",
+  },
+  {
+    name: "La Piazza",
+    lat: 41.33931,
+    lng: 14.50749,
+    audio: "../../media/audio/Piazza-Orticelli.mp3",
+    image: "../../media/photos/Piazza_orticelli.jpg",
+  },
+  {
+    name: "Le Campane",
+    lat: 41.33762,
+    lng: 14.50828,
+    audio: "../../media/audio/Le-Campane.mp3",
+    image: "../../media/photos/la_domenica.jpg",
+  },
+  {
+    name: "La Lavorazione Della Pietra",
+    lat: 41.35206,
+    lng: 14.52328,
+    audio: "../../media/audio/Lavorazione-del-Marmo.mp3",
+    image: "../../media/photos/Lavorazione_pietra.jpg",
+  },
+  {
+    name: "Alba Da Marcellino",
+    lat: 41.3495,
+    lng: 14.51957,
+    audio: "../../media/audio/Alba-Marcellino.mp3",
+    image: "../../media/photos/.jpg",
+  },
+  {
+    name: "Ponte Di Legno",
+    lat: 41.29995,
+    lng: 14.53883,
+    audio: "../../media/audio/Ponte-di-Legno-Forre.mp3",
+    image: "../../media/photos/Ponte_forre.jpg",
+  },
+  {
+    name: "Il Flauto \"A cocchia\"",
+    lat: 41.35306,
+    lng: 14.5078,
+    audio: "../../media/audio/flauto_5_edit.mp3",
+    image: "../../media/photos/il_flauto.jpg",
+  }
+];
+
